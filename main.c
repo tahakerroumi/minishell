77c3004f9441e4a7654753106400ce997abe775b
tkerroum <tkerroum@c4r9p6.1337.ma>
2024-08-17 21:37:41 +0100

    leaks done

--- main.c
+++ main.c
@@ -6,18 +6,30 @@
 /*   By: tkerroum <tkerroum@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/09 08:49:08 by ta7ino            #+#    #+#             */
-/*   Updated: 2024/08/17 11:50:13 by tkerroum         ###   ########.fr       */
+/*   Updated: 2024/08/17 21:35:16 by tkerroum         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-char *get_path(char **env)
+void	ft_free_double(char **str)
+{
+	int i = 0;
+	
+	while (str[i])
+	{
+		free(str[i]);
+		i++;
+	}
+	free(str);
+}
+
+char *get_path(char **env,char *str)
 {
     int i = -1;
     while (env[++i])
     {
-        if (ft_strncmp(env[i],"PATH",4) == 0)
+        if (ft_strncmp(env[i], str, ft_strlen(str)) == 0)
             return (env[i]);
     }
     return (NULL);
@@ -35,57 +47,57 @@ char **split_path(char *old_path)
 {
     int i = move_path(old_path);
     char **new_path = ft_split(old_path + i + 1, ':');
+	
     return (new_path);
 }
 
 char	*check_exec(char **path,char *command)
 {
 	int i = 0;
+	char *str;
 	while (path[i])
 	{
 		if (!access(command,F_OK))
+		{
+			ft_free_double(path);
 			return (command);
-		char *str = ft_strjoin(path[i], command);
+		}
+		str = ft_strjoin(path[i], command);
 		if (!access(str,F_OK))
+		{
+			ft_free_double(path);
+			free(command);
 			return (str);
+		}
 		free(str);
 		i++;
 	}
+	free(command);
+	ft_free_double(path);
 	return (NULL);
-}
-
-void	ft_free_double(char **str)
-{
-	int i = 0;
-	
-	while (str[i])
-	{
-		free(str[i]);
-		i++;
-	}
-	free(str);
 }
 
 void	executable(char **env, char *prompt)
 {
 	char *cmd;
-    char *envpath = get_path(env);
+    char *envpath = get_path(env,"PATH");
     char **path = split_path(envpath);
 	char **command = ft_split(prompt,' ');
 	cmd = ft_strjoin("/",command[0]);
 	char *exec = check_exec(path, cmd); // exec first arg;
 	if (!exec)
 	{
+		free(exec);
+		ft_free_double(command);
 		printf("%s: command not found\n",prompt);
-		free(cmd);
-		ft_free_double(path);
 		return ;
 	}
-	// free(cmd);
-	// ft_free_double(path);
-	int one = execve(exec,command,NULL);
-	if (one == -1)
+	if (execve(exec,command,env) == -1)
+	{
+		free(exec);
+		ft_free_double(command);
 		printf("%s command not found\n",prompt);
+	}
 }
 
 void	minishell(char **env, char *prompt)
